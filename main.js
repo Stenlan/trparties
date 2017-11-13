a15756174777f49942e014e85ef7a17f8936b7df
Stenlan <Stenlan@users.noreply.github.com>
2017-11-13 17:48:06 +0100

    Uploading locally created file

---
+++ main.js
@@ -0,0 +1,171 @@
+// ==UserScript==
+// @name         Typeracer Easier Parties
+// @namespace    com.github.stenlan.trparties
+// @version      0.1
+// @description  Typeracer easier party joining
+// @author       stenlan
+// @match        http://play.typeracer.com/*
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// @grant        GM_xmlhttpRequest
+// @connect      typeracer.com
+// ==/UserScript==
+
+var jQuerd = jQuery.noConflict(true);
+var previouslyOnMain = false;
+var previouslyInRoom = false;
+
+var zeInterval = setInterval(function(){
+    if(jQuerd(".mainMenu").first().length !== 0){
+        if(previouslyOnMain === false){
+            addRefreshButton();
+            checkMessages();
+            previouslyOnMain = true;
+        }
+    }else{
+        previouslyOnMain = false;
+    }
+    if(jQuerd(".urlTextbox.urlTextbox-readonly").val() !== undefined){
+        if(previouslyInRoom === false){
+            var roomUrl = jQuerd(".urlTextbox.urlTextbox-readonly").val();
+            if(roomUrl.startsWith("http://play.typeracer.com/?rt=")){
+                addInviteFriendsButton(jQuerd(".urlTextbox.urlTextbox-readonly").val().substr("http://play.typeracer.com/".length));
+                previouslyInRoom = true;
+            }
+        }
+    }else{
+        previouslyInRoom = false;
+    }
+}, 200);
+
+function checkMessages(){
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "http://data.typeracer.com/pit/messages",
+        onload: function(data){
+            handleMessageCheck(jQuerd(jQuerd.parseHTML(data.responseText)).find(".unreadMessage").first().find(".messageBody").first().children().first().html().trim());
+        }
+    });
+}
+
+function createJoinFriendTr(joinUrl){
+    if(jQuerd(".mainMenu").first().attr("joinfriendsbuttonadded") !== "true"){
+        var mainPlayButton = jQuerd(jQuerd.parseHTML(jQuerd(".mainMenu").first().find("table tbody").first().children("tr").get(1).outerHTML));
+        mainPlayButton.find("td.ImageButton").remove();
+        mainPlayButton.find("img").first().css("background", "url(https://i.imgur.com/UBn1nOJ.png) no-repeat 0px 0px");
+        mainPlayButton.find("img").first().click(function(){
+            window.location.replace(joinUrl);
+        });
+        mainPlayButton.find("a").first().html("Join friends");
+        mainPlayButton.find("a").first().attr("title", null);
+
+        mainPlayButton.find("a").first().click(function(){
+            window.location.replace(joinUrl);
+        });
+        mainPlayButton.find(".gwt-Label").html("A friend shared a track less than 5 minutes ago");
+        jQuerd(".mainMenu").first().find("table tbody").first().children("tr:eq(1)").after(mainPlayButton);
+        jQuerd(".mainMenu").first().attr("joinfriendsbuttonadded", "true");
+    }else{
+        jQuerd(".mainMenu").first().find("table tbody").first().children("tr:eq(2)").find("img").first().click(function(){
+            window.location.replace(joinUrl);
+        });
+        jQuerd(".mainMenu").first().find("table tbody").first().children("tr:eq(2)").find("a").first().click(function(){
+            window.location.replace(joinUrl);
+        });
+    }
+}
+
+function addRefreshButton(){
+    if(jQuerd(".mainMenu").first().attr("refreshbuttonadded") !== "true"){
+        var refreshTd = jQuerd(document.createElement("TD"));
+        refreshTd.css("vertical-align", "top");
+        refreshTd.css("margin-left", "20px");
+
+        refreshTd.attr("align", "left");
+        refreshTd.addClass("ImageButton");
+        refreshTd.click(function(){
+            checkMessages();
+        });
+
+        var refreshButton = jQuerd(document.createElement("IMG"));
+        refreshButton.css("width", "32");
+        refreshButton.css("height", "32");
+        refreshButton.attr("src", "https://i.imgur.com/Okx1nQg.png");
+
+        var refreshText = jQuerd(document.createElement("SPAN"));
+        refreshText.html("Refresh friend rooms");
+        refreshText.css("float","right");
+        refreshText.css("margin-left","10px");
+        refreshText.css("height","32px");
+        refreshText.css("line-height","32px");
+
+        refreshTd.append(refreshButton);
+        refreshTd.append(refreshText);
+        jQuerd(".mainMenu:eq(0) table:eq(0) tbody tr:eq(1) tbody tr:eq(0)").append(refreshTd);
+        jQuerd(".mainMenu").first().attr("refreshbuttonadded", "true");
+    }
+}
+
+function addInviteFriendsButton(roomUrl){
+    if(jQuerd(".roomSection").first().attr("invitefriendsbuttonadded") !== "true"){
+        var invFriendsButton = jQuerd(jQuerd.parseHTML(jQuerd(".roomSection table:eq(0) tbody:eq(0) tr:eq(1) tbody tr:eq(0)").children().get(1).outerHTML));
+        invFriendsButton.find("img").first().click(function(){
+            inviteFriends(roomUrl);
+        });
+        invFriendsButton.find("a").first().html("share with friends");
+        invFriendsButton.find("a").first().click(function(){
+            inviteFriends(roomUrl);
+        });
+        jQuerd(".roomSection table:eq(0) tbody:eq(0) tr:eq(1) tbody tr:eq(0)").children().eq(0).after(invFriendsButton);
+        jQuerd(".roomSection").first().attr("invitefriendsbuttonadded", "true");
+    }else{
+        jQuerd(".roomSection table:eq(0) tbody:eq(0) tr:eq(1) tbody tr:eq(0)").children().eq(1).find("a").first().html("share with friends");
+        jQuerd(".roomSection table:eq(0) tbody:eq(0) tr:eq(1) tbody tr:eq(0)").children().eq(1).find("img").first().click(function(){
+            inviteFriends(roomUrl);
+        });
+        jQuerd(".roomSection table:eq(0) tbody:eq(0) tr:eq(1) tbody tr:eq(0)").children().eq(1).find("a").first().click(function(){
+            inviteFriends(roomUrl);
+        });
+    }
+}
+
+function inviteFriends(roomUrl){
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "http://data.typeracer.com/pit/friends",
+        onload: function(data){
+            var friendsList = [];
+            jQuerd(jQuerd.parseHTML(data.responseText)).find(".friendsTable:eq(0) tbody:eq(0) tr[height]").each(function(index, element){
+                friendsList.push(jQuerd(element).find("td:eq(1) a").html());
+            });
+
+            friendsList.forEach(function(item, index){
+                GM_xmlhttpRequest({
+                    method: "POST",
+                    url: "http://data.typeracer.com/pit/message_compose",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    data: "to=" + item + "&body=" + roomUrl + "+" + Date.now() + "&returnUrl=http%3A%2F%2Fdata.typeracer.com%2Fpit%2Fmessages",
+                    onload: function(data2){
+                        if(index === friendsList.length - 1){
+                            alert("Successfully invited all friends!");
+                        }
+                    }
+                });
+            });
+        }
+    });
+}
+
+function handleMessageCheck(message){
+    if(message !== undefined){
+        message = message.split(" ");
+        timestamp = parseInt(message[1]);
+        if(message[0].startsWith("?rt=") && !isNaN(timestamp)){
+            jQuerd(".datarow tbody:eq(0) tr td:eq(2) img:eq(0)").css("background", "url(https://i.imgur.com/CLSgJUB.png) 0px 0px no-repeat");
+            if((Date.now() - timestamp) / 1000 < 300){
+                createJoinFriendTr("http://play.typeracer.com/" + message[0]);
+            }
+        }
+    }
+}
